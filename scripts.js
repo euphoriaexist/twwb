f3f651ff78b5527cd4cca0185f3fc7964bd2b4d4
cedar <87912944+euphoriaexist@users.noreply.github.com>
2025-10-27 12:52:38 -0400

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,10 @@
+document.getElementsByTagName("h1")[0].style.fontSize = "6vw";
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
